b7e5b07ee5241d4541bcdc5a2e1d361564a8bf72
Carlos Andres Ramirez Moreno <tyadr3s@gmail.com>
2026-02-10 15:57:29 -0500

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -69,5 +69,102 @@ namespace seneca {
       return cout;
    }
 
+   void Account::setInvalid() {
+      m_number = -1;
+      m_balance = 0.0;
+      m_holderName[0] = char(0);
+   }
+
+   Account::operator bool() const {
+      return m_holderName[0] && isValidNumber(m_number) && m_balance >= 0.0;
+   }
+
+   Account::operator int() const {
+      return m_number;
+   }
+
+   Account::operator double() const {
+      return m_balance;
+   }
+
+   Account::operator const char*() const {
+      return m_holderName;
+   }
+
+  char& Account::operator[](int index) {
+      int len = 0;
+      while (m_holderName[len]) len++;
+      if (index < 0) index = -index;
+      return m_holderName[len ? index % len : 0];
+   }
+
+  const char Account::operator[](int index) const {
+      int len = 0;
+      while (m_holderName[len]) len++;
+      if (index < 0) index = -index;
+      return m_holderName[len ? index % len : 0];
+   }
+
+   Account& Account::operator=(int number) {
+      if (~(*this))
+         isValidNumber(number) ? m_number = number : setInvalid();
+      return *this;
+   }
+
+   Account& Account::operator=(double balance) {
+      return balance >= 0 ? (m_balance = balance, *this)
+                          : (setInvalid(), *this);
+   }
+
+   Account& Account::operator+=(double amount) {
+      if (*this && amount >= 0) m_balance += amount;
+      return *this;
+   }
+
+   Account& Account::operator-=(double amount) {
+      if (*this && amount >= 0 && m_balance >= amount)
+         m_balance -= amount;
+      return *this;
+   }
+
+   Account& Account::operator<<(Account& right) {
+      if (this != &right && *this && right)
+         m_balance += right.m_balance, right.m_balance = 0;
+      return *this;
+   }
+
+   Account& Account::operator>>(Account& right) {
+      if (this != &right && *this && right)
+         right.m_balance += m_balance, m_balance = 0;
+      return *this;
+   }
+
+   bool Account::operator~() const {
+      return m_number == 0 && m_holderName[0];
+   }
+
+   Account& Account::operator++() {
+      if (*this) m_balance += 1;
+      return *this;
+   }
+
+   Account Account::operator++(int) {
+      Account old = *this;
+      ++(*this);
+      return old;
+   }
+
+   Account& Account::operator--() {
+      if (*this && m_balance >= 1) m_balance -= 1;
+      return *this;
+   }
+
+   Account Account::operator--(int) {
+      Account old = *this;
+      --(*this);
+      return old;
+   }
+
+
    
 }
