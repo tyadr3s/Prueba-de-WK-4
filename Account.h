ac6e8070141521cd70c401b9924bb85cdcdc123f
Carlos Andres Ramirez Moreno <tyadr3s@gmail.com>
2026-02-10 15:52:46 -0500

    Update Account.h

--- Account.h
+++ Account.h
@@ -5,19 +5,39 @@
 namespace seneca {
    const int NameMaxLen = 30;
    class Account {
-      double m_balance;
-      int m_number;
-      char m_holderName[NameMaxLen + 1];
-      void cpyName(const char* src); // copies the src into the m_holderName array
-      bool isValidNumber(int number)const; // returns true if number is a valid account number
+      double m_balance{};
+      int m_number{};
+      char m_holderName[NameMaxLen + 1]{};
+
+      void cpyName(const char* src);
+      bool isValidNumber(int number)const;
+      void setInvalid();            
+
    public:
       Account(const char* holderName = nullptr);
       Account(const char* holderName, int number, double balance);
       std::ostream& display()const;
 
+      operator bool() const;
+      operator int() const;
+      operator double() const;
+      operator const char*() const;
 
+      char& operator[](int index);
+      const char operator[](int index) const;
+
+      Account& operator=(int number);
+      Account& operator=(double balance);
+
+      Account& operator+=(double amount);
+      Account& operator-=(double amount);
+      Account& operator<<(Account& right);
+      Account& operator>>(Account& right);
+
+      bool operator~() const;
+      Account& operator++();   Account operator++(int);
+      Account& operator--();   Account operator--(int);
    };
-  
-
 }
-#endif // SENECA_ACCOUNT_H_
+
+#endif
